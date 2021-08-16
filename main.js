4aae7ac9cf18f7402e9381d8c9e539ee1c264403
OlivierZoungrana <synthejazz@gmail.com>
2021-08-16 20:03:04 +0200

    mis en place de la confirmation de commande

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 
 const carts = document.querySelector("#cart");
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const productId = urlParams.get('sku');
+const productSpan = document.getElementById('product_id');
+
+let boutique = document.querySelector(".boutique")
+
+ if(productId)
+ getProductDetails(productId);
+if(boutique && productId===null)
+getProducts();
 
 
 
@@ -165,17 +176,6 @@ fetch("http://localhost:3000/api/cameras/"+ productId)
 };
 
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const productId = urlParams.get('sku');
-const productSpan = document.getElementById('product_id');
-
-let boutique = document.querySelector(".boutique")
-
- if(productId)
- getProductDetails(productId);
-if(boutique && productId===null)
-getProducts();
 
 
 function generateOneCamera( idCamera,nameCamera,priceCamera,imageUrl, descriptionCamera, selectionCamera, onClick={} ){
@@ -261,69 +261,6 @@ function generateOneCamera( idCamera,nameCamera,priceCamera,imageUrl, descriptio
     displayCart();
 
 
-    const afficherFormulaire =() =>{
-
-        const positionFormulaire = document.querySelector(".formulaire-content")
-
-        const structureFormulaire = `
-
-        <form>
-          <div class="form-row">
-            <div class="col-md-4 mb-3">
-              <label for="nom">Nom</label>
-              <input type="text" class="form-control is-valid" id="firstName" placeholder="First name" value="Mark" required>
-              <div class="valid-feedback">
-                Valide!
-              </div>
-            </div>
-            <div class="col-md-4 mb-3">
-              <label for="prenom">Prénom</label>
-              <input type="text" class="form-control is-valid" id="lastName" placeholder="Last name" value="Otto" required>
-              <div class="valid-feedback">
-                Valide!
-              </div>
-            </div>
-            <div class="col-md-6 mb-3">
-              <label for="adresse">Adresse</label>
-              <input type="text" class="form-control is-valid" id="address" placeholder="Adresse" required>
-              <div class="invalid-feedback">
-                Entrer votre Adresse.
-              </div>
-            </div>
-          </div>
-          <div class="form-row">
-            <div class="col-md-6 mb-3">
-              <label for="ville">Ville</label>
-              <input type="text" class="form-control is-valid" id="city" placeholder="City" required>
-              <div class="invalid-feedback">
-                Choissisez une ville.
-              </div>
-            </div>
-          </div>
-          <div class="form-row">
-            <div class="col-md-6 mb-3">
-              <label for="email">E-mail</label>
-              <input type="email" class="form-control is-valid" id="email" placeholder="e-mail" required>
-              <div class="invalid-feedback">
-                Votre 
-              </div>
-            </div>
-          </div>
-  
-          <button id="sendFormulaire" class="btn btn-primary" type="submit">Soumettre</button>
-        </form>
-          
-        
-        
-        
-        `;
-
-        positionFormulaire.insertAdjacentHTML("afterend", structureFormulaire);
-    }
-
-afficherFormulaire();
-
-
 //création tableauId
 
 function createTableauId(){
@@ -361,17 +298,129 @@ e.preventDefault();
     email: document.querySelector("#email").value
  }
 
- localStorage.setItem("contact", JSON.stringify(contact))
+
+ // validation formulaire//
+
+ let testAlert = (value)=>{
+    return `${value} n'est pas correct:  Chiffres et symboles ne sont aps autorisés \n ne pas dépasser 20caractères, minimum 3 caractères ` }
+
+
+ let regExLastNameFistNameCity = (value)=>{
+   return /^[ \u00c0-\u01ffa-zA-Z'\-]{3,20}$/.test(value);
+ }
+
+ let regExEmail = (value)=>{
+  return /^^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+ }
+
+ let regExAdress = (value)=>{
+   return /^[a-zA-Z0-9\s,.'-]{3,}$/.test(value);
+ }
+ //controle de la validité du nom
+
+
+ // gérer l'affichage du texte à coté des champs
+ function dataChampmanquantTexteVide(querySelectorId){
+  document.querySelector(`#${querySelectorId}`).textContent ="";
+ }
+
+ function dataChampmanquantTexte(querySelectorId){
+  document.querySelector(`#${querySelectorId}`).textContent ="veuillez remplir correctement ce chanp";
+ }
+
+ function controlFirstName(){
+
+ let theFistName = contact.firstName
+
+ if(regExLastNameFistNameCity(theFistName)){
+  dataChampmanquantTexteVide("noFirstName");
+  return true;
+ }else{
+   dataChampmanquantTexte("noFirstName");
+alert(testAlert("fistName"))
+ return false;
+}
+ }
+
+function controlLastName(){
+  let theLastName = contact.lastName
+
+ if(regExLastNameFistNameCity(theLastName)){
+   dataChampmanquantTexteVide("noLastName")
+  return true;
+ }else{
+ dataChampmanquantTexte("noLastName")
+alert(testAlert("lastName"))
+ return false;
+}
+}
+
+function controlCity(){
+  let theCity = contact.city
+
+ if(regExLastNameFistNameCity(theCity)){
+   dataChampmanquantTexteVide("noCity")
+  return true;
+ }else{
+ dataChampmanquantTexte("noCity")
+alert(testAlert("city"))
+ return false;
+}
+}
+function controlAdresse(){
+
+  let theAdresse = contact.address
+
+ if(regExAdress(theAdresse)){
+   dataChampmanquantTexteVide("noAddress")
+  return true;
+ }else{
+ dataChampmanquantTexte("noAddress")
+alert("l' adresse n'est pas valide")
+ return false;
+}
+
+}
+
+function controlEmail(){
+
+  let theEmail = contact.email
+
+ if(regExEmail(theEmail)){
+   dataChampmanquantTexteVide("noEmail")
+  return true;
+ }else{
+ dataChampmanquantTexte("noEmail")
+alert("l'email n'est pas valide")
+ return false;
+}
+
+}
+
+
+if(controlFirstName() && controlLastName() && controlCity() && controlEmail() && controlAdresse()){
+ localStorage.setItem("contact", JSON.stringify(contact));
+
+
 
     const aEnvoyer = {
         products,
         contact
-    }
+    };
 
-    console.log(aEnvoyer);
+    sendToServer(aEnvoyer);
+
+    
+}else{
+  alert("remplissez correctement le formulaire");
+}
+
+    
+});
 
 
-    let promesse = fetch("http://localhost:3000/api/cameras/order/", {
+function sendToServer(aEnvoyer){
+  let promesse = fetch("http://localhost:3000/api/cameras/order/", {
         method: "POST",
         body: JSON.stringify(aEnvoyer),
         headers: {
@@ -379,32 +428,48 @@ e.preventDefault();
         },
 
     });
-await promesse.then((res)=>{
-    
-    return res.json()
-    
-    }).then((data)=> console.log(data)).catch(console.log)
-    // console.log("promesse")
-    // console.log(promesse)
 
-  
-})
+    promesse.then(async(response)=>{
+      try{
+        let contenu = await response.json();
+        console.log("contenu de la reponse");
+        console.log(contenu);
+
+if(response.ok){
+
+  //récuperer lId
+  console.log("l'orderId")
+  console.log(contenu.orderId);
+  localStorage.setItem("orderId", contenu.orderId);
+
+  //Changer de page
+
+  window.location = "order.html";
+
+}else{
+
+}
+
+      }catch(e){
+        console.log(e)
+      }
+    });
+
+}
+//contenu du résultat serveur
 
 
 
 // contenu local storage dans le formulaire
 
-
-
 let localData = localStorage.getItem("contact")
+let dataObject = JSON.parse(localData);
 
-console.log(localData)
-
-let dataObject = JSON.parse(localData)
+// validation du formulaires 
 
 function remplirFormulaireLocalStorage(input){
-document.querySelector(`#${input}`).value = dataObject[input];
-}
+  document.querySelector(`#${input}`).value = dataObject[input];
+};
 
 remplirFormulaireLocalStorage("firstName");
 remplirFormulaireLocalStorage("lastName");
@@ -414,7 +479,47 @@ remplirFormulaireLocalStorage("email");
 
 
 console.log(dataObject)
+
+
+
+
+// function displayCommandeConfirmation(){
+
+//   let order = localStorage.getItem("contact")
+
+//   order = JSON.parse(order)
+
+//   console.log(order)
+
+//   let orderContainer = document.querySelector(".orderClient")
+
+//   if(order && orderContainer){
+
+//     Object.values(order).map(item=>{
+
+//       orderContainer += `
+      
+//       <div>
+//       <h2>Mr${item} votre commande est validé </h2>
+//       </div>
+//       `
+    
+    
+//     })
+//   }
+
+// }
+
+// displayCommandeConfirmation();
        
+
+
+// récuper lorderID
+
+
+
+
+
 
 
 
---
+++ order.js
@@ -0,0 +1,29 @@
+
+
+let orderIdResponse = localStorage.getItem("orderId");
+
+console.log(orderIdResponse)
+// récuperer le prix total
+
+let prixTotalResponse = localStorage.getItem("cart")
+
+prixTotalResponse = JSON.parse(prixTotalResponse)
+
+
+let positionConfirmationOrder = document.querySelector("#recapCommande")
+
+let structureConfCommande = `
+<div class="card">
+<div class="card-title">
+  <h2>Recap Commande</h2>
+</div>
+<div class="card-text">
+  <p>Merci pour votre commande</p>
+  <p>Votre commande numero: <span class="gras">${orderIdResponse}</span> a bien été prise en compte</p>
+  <p>Le montant de votre commande est de <span class="gras">${prixTotalResponse["total"]}</span> Euros</p>
+  <p>Au plaisir de vous revoir bientôt</p>
+</div>
+</div>
+`;
+
+positionConfirmationOrder.insertAdjacentHTML("afterend",structureConfCommande);
